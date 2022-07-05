7151e3e8af1444b5064548b3bc6230b1e857369c
Romain Tartière <romain@blogreen.org>
2022-07-05 09:54:27 -1000

    Improve error reporting

--- tasks/set_puppet_config.rb
+++ tasks/set_puppet_config.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'open3'
-
 require_relative '../../ruby_task_helper/files/task_helper'
 
 class SetPuppetConfig < TaskHelper
@@ -14,7 +12,7 @@ class SetPuppetConfig < TaskHelper
     end
 
     settings.each do |setting_name, setting_value|
-      Open3.capture3('puppet', 'config', 'set', setting_name.to_s, setting_value.to_s)
+      system('puppet', 'config', 'set', setting_name.to_s, setting_value.to_s) || raise(TaskHelper::Error.new('Failed to set setting', 'set_puppet_config', 'puppet exited with a non-null error code'))
     end
 
     nil
