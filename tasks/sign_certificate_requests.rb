a9aadbd52c5ee850a2638b1278958b2348bccc67
Romain Tartière <romain@blogreen.org>
2024-09-16 08:38:39 -1000

    Fix AIO path for PuppetServer
    The AIO Puppet Server is installed in /opt/puppetlabs/server,
    /opt/puppetlabs/puppet being the path of the Agent.
    
    Both Agent and Server binaries have symlinks in /opt/puppetlabs/bin, it
    is this directory that we want to make sure is part of $PATH before
    running `puppetserver ca list` and `puppetserver ca sign`.

--- tasks/sign_certificate_requests.rb
+++ tasks/sign_certificate_requests.rb
@@ -6,9 +6,9 @@ require_relative '../../ruby_task_helper/files/task_helper'
 class SignCertificateRequests < TaskHelper
   def task(certificate_requests:, **_kwargs)
     # Prepend AIO path if it exist and is not in $PATH
-    if File.directory?('/opt/puppetlabs/puppet/bin') &&
-       !ENV.fetch('PATH').split(':').include?('/opt/puppetlabs/puppet/bin')
-      ENV['PATH'] = "/opt/puppetlabs/puppet/bin:#{ENV.fetch('PATH')}"
+    if File.directory?('/opt/puppetlabs/bin') &&
+       !ENV.fetch('PATH').split(':').include?('/opt/puppetlabs/bin')
+      ENV['PATH'] = "/opt/puppetlabs/bin:#{ENV.fetch('PATH')}"
     end
 
     certificate_requests.each do |node, details|
